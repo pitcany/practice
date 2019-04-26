52a9f65d2873cc7e4a2ce37ef476124c58b712cc
Yannik <pitcany@gmail.com>
2019-04-26 16:27:35 -0700

    inversions

--- inversions.py
+++ inversions.py
@@ -8,7 +8,6 @@ Created on Fri Apr 26 10:28:22 2019
 
 # Uses python3
 import sys
-from bisect import bisect_left
 
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
@@ -21,24 +20,27 @@ def get_number_of_inversions(a, b, left, right):
     # merge the halves together to get a sorted list
     # also count number of inversions where x in first half > y in second half
     i,j,k = left,ave,0
-    for x in b[left:ave]:
-        number_of_inversions += bisect_left(b[ave:right],x)
-    temp = b[left:right]
-    while ((b[i:ave] or b[j:right]) and k <= right-left-1):
-        if not b[i:ave]:
+
+    temp = [0]*(right-left)
+    while ((i<ave or j<right) and k <= right-left-1):
+        if i==ave:
             temp[k]=b[j]
-        elif not b[j:right]:
+            j += 1
+        elif j==right:
             temp[k]=b[i]
+            i += 1
         elif b[i]<=b[j]:
             temp[k]=b[i]
-            i+=1
+            i += 1
         elif b[i]>b[j]:
             temp[k]=b[j]
-            j+=1
+            number_of_inversions += ave-i
+            j += 1
         k += 1
     b[left:right] = temp
     return number_of_inversions
 
+#get_number_of_inversions([1,4,2,3,8,5],[0,0,0,0,0,0],0,6)
 #get_number_of_inversions([2,3,9,2,9],[0,0,0,0,0],0,5)
 #get_number_of_inversions([4,3,2,1],[0,0,0,0],0,4)
 
