7a4306f0d04173a384cce5785db14148dd23398e
Yannik Pitcan <yannik.pitcan@performance-star.com>
2023-10-27 21:49:48 -0700

    negative cycle fixed

--- coursera exercises/negative_cycle.py
+++ coursera exercises/negative_cycle.py
@@ -1,9 +1,9 @@
 #Uses python3
 
 import sys
+from collections import deque
 
-
-def negative_cycle(adj, cost):
+def negative_cycle1(adj, cost):
     for src in range(len(adj)):
         distance = [10**19]*(len(adj))
         distance[src] = 0
@@ -18,6 +18,30 @@ def negative_cycle(adj, cost):
             for v, c in zip(adj[u], cost[u]):
                 if distance[u] + c < distance[v] and distance[u] != 10**19:
                     return 1
+    return 0
+
+def negative_cycle(adj, cost):
+    for src in range(len(adj)):
+        distance = [10**19] * len(adj)
+        distance[src] = 0
+        active_vertices = deque([src])
+        visited = [False] * len(adj)
+        visited[src] = True
+        relaxations = 0
+        
+        while active_vertices:
+            u = active_vertices.popleft()
+            visited[u] = False
+            for v, c in zip(adj[u], cost[u]):
+                if distance[u] + c < distance[v] and distance[u] != 10**19:
+                    distance[v] = distance[u] + c
+                    if not visited[v]:
+                        active_vertices.append(v)
+                        visited[v] = True
+                        relaxations += 1
+                        if relaxations >= len(adj) * len(adj):
+                            return 1
+    
     return 0
 
 if __name__ == '__main__':
